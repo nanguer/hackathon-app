4d9a278a596b5227006d3002fce30a1586715a06
nanguer <nahuelgimenez18@gmail.com>
2020-01-05 20:28:39 +0100

    remove gitignore files

--- Capstone-NOV/client/src/actions/navigation.js
+++
@@ -1,5 +0,0 @@
-export const backClick = (isAdmin, push, routeAdmin, routeUser) => {
-  let backRoute = '/';
-  isAdmin ? (backRoute = routeAdmin) : (backRoute = routeUser);
-  push(backRoute);
-};
--- Capstone-NOV/client/src/actions/types.js
+++
@@ -1,8 +0,0 @@
-export const GET_ERRORS = 'GET_ERRORS';
-export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-export const SET_ADMIN = 'SET_ADMIN';
-export const GET_IDEAS = 'GET_IDEAS';
-export const SET_IDEAS = 'SET_IDEAS';
-export const ERASE_IDEA = 'ERASE_IDEA';
-export const ADD_TEAM = 'ADD_TEAM';
-export const SET_ALL_EVENTS = 'SET_ALL_EVENTS';
